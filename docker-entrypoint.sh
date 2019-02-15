d5147f6b9fa1c7603c78b48b787d8a30f85f95d3
Michał Sitko <pierwszy1@gmail.com>
2019-02-15 11:51:04 +0100

    enable mounting local .ivy2 cache

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# The only point of that entrypoint file is to merge `ivy2_image/local` (where sbt libraries built in Dockerfile reside)
+# into `/root/.ivy2/local`. `.ivy2/local` is where image user could mount his host ivy cache
+
+set -e
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+mkdir -p ~/.ivy2/local
+
+if [ ! -d  /root/.ivy2/local/com.tgf.pizza ]; then
+	mv /root/.ivy_image/local/com.tgf.pizza /root/.ivy2/local
+else
+	printf "${RED}WARN: ${NC}On your host directory ~/.ivy2/local/com.tgf.pizza exists. Your host versions of libraries will be used instead of ones from image.\n"
+fi
+
+sbt $@
