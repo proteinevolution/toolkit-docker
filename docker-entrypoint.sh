3fac38525cf873e1047387f38e3407173084bc6f
Felix Gabler <felix.gabler98@gmail.com>
2019-02-15 18:10:19 +0100

    release v1.1.5

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,15 +2,15 @@
 
 # The only point of that entrypoint file is to merge `ivy2_image/local` (where sbt libraries built in Dockerfile reside)
 # into `/root/.ivy2/local`. `.ivy2/local` is where image user could mount his host ivy cache
+# IMPORTANT: always use LF line endings for this file
 
 set -e
-
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 mkdir -p ~/.ivy2/local
 
-if [ ! -d  /root/.ivy2/local/com.tgf.pizza ]; then
+if [[ ! -d  /root/.ivy2/local/com.tgf.pizza ]]; then
 	mv /root/.ivy_image/local/com.tgf.pizza /root/.ivy2/local
 else
 	printf "${RED}WARN: ${NC}On your host directory ~/.ivy2/local/com.tgf.pizza exists. Your host versions of libraries will be used instead of ones from image.\n"
